654047b6977dae837ab9d3118dadf9f949be7d91
kevin lee <leem43517@gmail.com>
2025-04-13 23:14:06 -0700

    Troubleshoot CodeDeploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Set proper permissions
-echo "Setting file permissions..."
+echo ">> Setting file permissions..." | logger -t codedeploy
 chmod -R 755 /var/www/html/ccp
 chown -R ec2-user:ec2-user /var/www/html/ccp
 
 # Restart Nginx
-echo "Restarting Nginx..."
+echo ">> Restarting Nginx..." | logger -t codedeploy
 systemctl restart nginx
 
 # Ensure Nginx starts on boot
 systemctl enable nginx
 
-echo "Deployment completed successfully!"
+echo ">> Deployment completed successfully!" | logger -t codedeploy
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Clean the target directory
-echo "Cleaning target directory..."
+echo ">> Cleaning target directory..." | logger -t codedeploy
 rm -rf /var/www/html/ccp/*
 echo "Directory cleaned."
